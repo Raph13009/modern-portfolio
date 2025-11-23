ab822b623ee10b9fab7eb02eb923dbc0c784ab66
Raph13009 <raphaellevy027@gmail.com>
2025-11-23 12:53:09 +0000

    Fix scrolling: restore fixed fullscreen sections behavior

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -12,7 +12,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <AnimatePresence mode="wait">
-        <motion.div key={router.route} className="h-full">
+        <motion.div key={router.route} className="h-full overflow-hidden">
           <Transition />
           <Component {...pageProps} />
         </motion.div>
